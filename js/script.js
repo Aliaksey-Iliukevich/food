142776c3de6315213ffb66a422254f896c730ac2
Alex <aleks.ilykevich@gmail.com>
2022-01-19 20:21:41 +0300

    refactor: script.js

--- js/script.js
+++ js/script.js
@@ -274,12 +274,13 @@ window.addEventListener('DOMContentLoaded', function() {
         .then(res => console.log(res));
 
         const slides = document.querySelectorAll(`.offer__slide`),
+              slider = document.querySelector(`.offer__slider`),
               prev = document.querySelector(`.offer__slider-prev`),
               next = document.querySelector(`.offer__slider-next`),
               total = document.querySelector(`#total`),
               current = document.querySelector(`#current`),
               slidesWrapper =document.querySelector(`.offer__slider-wrapper`),
-              skidesField = document.querySelector(`.offer__slider-inner`),
+              slidesField = document.querySelector(`.offer__slider-inner`),
               width = window.getComputedStyle(slidesWrapper).width;
 
         let slideIndex = 1,
@@ -293,9 +294,9 @@ window.addEventListener('DOMContentLoaded', function() {
             current.textContent = slideIndex;
         }
 
-        skidesField.style.width = 100 * slides.length + `%`;
-        skidesField.style.display = `flex`;
-        skidesField.style.transition = `0.5s all`;
+        slidesField.style.width = 100 * slides.length + `%`;
+        slidesField.style.display = `flex`;
+        slidesField.style.transition = `0.5s all`;
 
         slidesWrapper.style.overflow = `hidden`;
 
@@ -303,44 +304,117 @@ window.addEventListener('DOMContentLoaded', function() {
             slide.style.width = width;
         });
 
+        slider.style.position = 'relative';
+
+        const indicators = document.createElement('ol'),
+              dots = [];
+        indicators.classList.add('carousel-indicators');
+        indicators.style.cssText = `
+            position: absolute;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            z-index: 15;
+            display: flex;
+            justify-content: center;
+            margin-right: 15%;
+            margin-left: 15%;
+            list-style: none;
+        `;
+        slider.append(indicators);
+    
+        for (let i = 0; i < slides.length; i++) {
+            const dot = document.createElement('li');
+            dot.setAttribute('data-slide-to', i + 1);
+            dot.style.cssText = `
+                box-sizing: content-box;
+                flex: 0 1 auto;
+                width: 30px;
+                height: 6px;
+                margin-right: 3px;
+                margin-left: 3px;
+                cursor: pointer;
+                background-color: #fff;
+                background-clip: padding-box;
+                border-top: 10px solid transparent;
+                border-bottom: 10px solid transparent;
+                opacity: .5;
+                transition: opacity .6s ease;
+            `;
+            if (i == 0) {
+                dot.style.opacity = 1;
+            }
+            indicators.append(dot);
+            dots.push(dot);
+        }
+
+        function slideNumber() {
+            if (slides.length < 10){
+                current.textContent = `0${slideIndex}`;
+        }else{
+            current.textContent = slideIndex;
+        }
+        }
+
+        function changeDot() {
+            dots.forEach(dot => dot.style.opacity = ".5");
+            dots[slideIndex-1].style.opacity = 1;
+        }
+
+        function onlyNumbers(str){
+            return +str.replace(/\D/g, ``);
+        }
+
         next.addEventListener(`click`, () => {
-            if(offset == +width.slice(0, width.length -2) * (slides.length - 1)){
+            if(offset == onlyNumbers(width) * (slides.length - 1)){
                 offset = 0;
             } else{
-                offset += +width.slice(0, width.length -2);
+                offset += onlyNumbers(width);
             }
 
-            skidesField.style.transform = `translateX(-${offset}px)`;
+            slidesField.style.transform = `translateX(-${offset}px)`;
 
                 if (slideIndex == slides.length){
                     slideIndex = 1;
                 } else{
                     slideIndex++;
                 }
-                    if (slides.length < 10){
-                            current.textContent = `0${slideIndex}`;
-                    }else{
-                        current.textContent = slideIndex;
-                    }
+                    slideNumber();
+                    changeDot();
         });
         
         prev.addEventListener(`click`, () => {
             if(offset == 0){
-                offset = +width.slice(0, width.length -2) * (slides.length - 1);
+                offset = onlyNumbers(width) * (slides.length - 1);
             } else{
-                offset -= +width.slice(0, width.length -2);
+                offset -= onlyNumbers(width);
             }
-            skidesField.style.transform = `translateX(-${offset}px)`;
+            slidesField.style.transform = `translateX(-${offset}px)`;
                 if (slideIndex == 1){
                     slideIndex = slides.length;
                 } else{
                     slideIndex--;
                 }
-                        if (slides.length < 10){
-                            current.textContent = `0${slideIndex}`;
-                    }else{
-                        current.textContent = slideIndex;
-                    }
+                slideNumber();
+                changeDot();
+        });
 
+        dots.forEach(dot => {
+            dot.addEventListener('click', (e) => {
+                const slideTo = e.target.getAttribute('data-slide-to');
+    
+                slideIndex = slideTo;
+                offset = onlyNumbers(width) * (slideTo - 1);
+    
+                slidesField.style.transform = `translateX(-${offset}px)`;
+    
+                if (slides.length < 10) {
+                    current.textContent =  `0${slideIndex}`;
+                } else {
+                    current.textContent =  slideIndex;
+                }
+    
+                changeDot();
+            });
         });
 });
